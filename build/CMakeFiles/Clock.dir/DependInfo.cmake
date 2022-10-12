00516c0e63a6ccfbdc14b3b318b2a23bfd3bd976
jerrykress <jerrykress.wsj@gmail.com>
2022-10-12 23:16:54 +0100

    Add Font

---
+++ build/CMakeFiles/Clock.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Common/Common.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Common/Common.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Common/Common.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Common/Pixel.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Common/Pixel.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Common/Pixel.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Common/Stylable.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Common/Stylable.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Common/Stylable.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Display/BaseDisplayObject.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Display/BaseDisplayObject.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Display/BaseDisplayObject.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Display/Display.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Display/Display.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Display/Display.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Display/Layout.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Display/Layout.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Display/Layout.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Shapes/Circle.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Circle.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Circle.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Shapes/CompositeShape.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/CompositeShape.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/CompositeShape.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Shapes/Ellipse.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Ellipse.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Ellipse.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Shapes/GenericShapeObjcet.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/GenericShapeObjcet.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/GenericShapeObjcet.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Shapes/Line.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Line.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Line.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Shapes/Rectangle.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Rectangle.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Shapes/Rectangle.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Utils/Converter.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Utils/Converter.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Utils/Converter.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Utils/Logger.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Utils/Logger.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Utils/Logger.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Widgets/GenericTextObject.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Widgets/GenericTextObject.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Widgets/GenericTextObject.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Widgets/TextField.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Widgets/TextField.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Widgets/TextField.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Window/DataWindow.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Window/DataWindow.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Window/DataWindow.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Window/GenericWindowObject.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Window/GenericWindowObject.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Window/GenericWindowObject.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/Xcurse/src/Window/GridWindow.cpp" "CMakeFiles/Clock.dir/Xcurse/src/Window/GridWindow.cpp.o" "gcc" "CMakeFiles/Clock.dir/Xcurse/src/Window/GridWindow.cpp.o.d"
+  "/home/jerry/Code/ASCII-Clock/clock.cpp" "CMakeFiles/Clock.dir/clock.cpp.o" "gcc" "CMakeFiles/Clock.dir/clock.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
